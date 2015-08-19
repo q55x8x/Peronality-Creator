ef4852cf33ca199785b4b6c566baf4f4d3d5774f
png2 <pngfou@yahoo.fr>
2015-08-19 15:09:42 +0200

    remember the opened personas and the last selected path

---
+++ Personality Creator/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Personality_Creator
+{
+    internal class Settings
+    {
+        public List<string> openedPersonas
+        {
+            get;
+        }
+        public string lastDir {
+            get;
+            set;
+        }
+
+        public Settings()
+        {
+            string openedPersonas = Properties.Settings.Default.openedPersonas;
+            this.openedPersonas = new List<string>(openedPersonas.Split(','));
+
+            this.lastDir = Properties.Settings.Default.lastDir;
+        }
+
+        public void save()
+        {
+            Properties.Settings.Default.openedPersonas = string.Join(",", this.openedPersonas.ToArray());
+            Properties.Settings.Default.lastDir = this.lastDir;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
