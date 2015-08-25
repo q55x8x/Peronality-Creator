a643d8afc9135e1875fa28cc3f7241f3c6f6ab7a
png2 <pngfou@yahoo.fr>
2015-08-25 15:20:45 +0200

    select the whole targeted line on ctl+click on @Goto

--- Personality Creator/frmMain.cs
+++ Personality Creator/frmMain.cs
@@ -737,7 +737,7 @@ namespace Personality_Creator
                 string gotoName = Match(this.CurrentEditor.GetLineText(p.iLine), @"(?i)(?<=\@goto\(|then\()[A-z_0-9öäüáéíóú+\s]+(?=\))").Value; //extracting the goto specifier
                 int index = Match(this.CurrentEditor.Text, $@"(?<=\n)\({gotoName}\)").Index; //finding the goto destination
                 Range range = this.CurrentEditor.GetRange(index + gotoName.Length + 2, index + gotoName.Length + 2);
-                this.currentEditor.Selection = range;
+                this.currentEditor.Selection = new Range(this.currentEditor, range.Start.iLine);
                 this.currentEditor.DoCaretVisible();
             }
         }
